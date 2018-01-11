426c51a232aa10dc2a1abae70c25c88c0ad488f8
Eric Zeng <leizeng@leizeng.local>
2018-01-11 16:16:59 +0800

    [Lei]name convention

--- src/atoms/Colors/Brand.tsx
+++ src/atoms/Colors/Brand.tsx
@@ -2,22 +2,22 @@ import * as React from 'react';
 import styled, { css } from 'styled-components';
 import { getPaletteFromTheme } from '../../themes';
 
-type TColorProps = {
+export type TBrandColorProps = {
   theme: ITheme;
   palette: string;
   width?: string;
   height?: string;
 };
 
-const backgroundColor = (props: TColorProps) => getPaletteFromTheme(props.palette);
+const backgroundColor = (props: TBrandColorProps) => getPaletteFromTheme(props.palette);
 
 const styles = css`
   display: block;
-  width: ${({width = '200px'}: TColorProps) => width};
-  height: ${({height = '40px'}: TColorProps) => height};
+  width: ${({width = '200px'}: TBrandColorProps) => width};
+  height: ${({height = '40px'}: TBrandColorProps) => height};
   background: ${backgroundColor};
 `;
 
 const StyledBrandColor = styled.span`${styles}`;
 
-export default (props: TColorProps) => (<StyledBrandColor {...props}/>);
+export default (props: TBrandColorProps) => (<StyledBrandColor {...props}/>);
