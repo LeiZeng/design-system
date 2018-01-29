1e4ae6bb1a8e11b7030bfe65c3ebe51a3e202725
Eric Zeng <leizeng@leizeng.local>
2018-01-29 16:22:53 +0800

    input text and textarea

--- src/components/atoms/Input/Input.md
+++ src/components/atoms/Input/Input.md
@@ -1,6 +1,6 @@
 ```
 [
-  <Input key="default" />,
+  <Input key="default" placeholder="Text something..." />,
   <Input key="select" type="select" options={{a: 'Option One', b: 'Option Two'}}/>,
   <Input key="textarea" type="textarea" height={100}/>,
   <Input key="checkbox" type="checkbox" />,
--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -25,9 +25,12 @@ interface IRadioProps extends IInputProps {
 }
 type TInputPropsAll = ISelectProps | IRadioProps | ITextProps;
 
+const DEFAULT_TYPE = 'text';
 const DEFAULT_HEIGHT = 40;
-const fontSize = ({ height = DEFAULT_HEIGHT }: IInputProps) =>
-  `${height / 35.5555555556}rem`;
+const fontSize = ({
+  type = DEFAULT_TYPE,
+  height = DEFAULT_HEIGHT
+}: TInputPropsAll) => type === 'textarea' ? '1rem' : `${height / 35.5555555556}rem`;
 
 const styles = css`
   font-family: ${font('primary')};
@@ -36,19 +39,24 @@ const styles = css`
   margin: 0;
   box-sizing: border-box;
   font-size: ${fontSize};
-  padding: ${ifProp(
-    { type: 'textarea' },
-    '0.4444444444em',
-    '0 0.4444444444em'
-  )};
-  height: ${ifProp({ type: 'textarea' }, 'auto', '2.2222222222em')};
+  padding: ${ifProp({ type: 'textarea' }, '0.4444444444em', '0 0.4444444444em')};
+  height: ${({ height }) => ifProp({ type: 'textarea' }, `${height}px`, '2.2222222222em')};
   color: ${palette('grayscale', 0)};
   background-color: ${palette('grayscale', 0, true)};
-  border: 1px solid
-    ${ifProp('invalid', palette('danger', 2), palette('grayscale', 3))};
+  border: 1px solid ${ifProp('invalid', palette('danger', 2), palette('grayscale', 2, true))};
   border-radius: 2px;
-  &[type='checkbox'],
-  &[type='radio'] {
+  outline: 0;
+  &::placeholder {
+    color: ${palette('grayscale', 3, true)}
+  }
+  &:hover {
+    border: 1px solid ${ifProp('invalid', palette('danger', 2), palette('primary', 2))};
+  }
+  &:focus {
+    border: 1px solid ${ifProp('invalid', palette('danger', 2), palette('primary', 2))};
+    box-shadow: 0 0 2px 0 ${ifProp('invalid', palette('danger', 2), palette('primary', 2))}
+  }
+  &[type=checkbox], &[type=radio] {
     display: inline-block;
     border: 0;
     border-radius: 0;
@@ -57,10 +65,10 @@ const styles = css`
     margin: 0 0.2rem 0 0;
   }
 `;
-const StyledTextarea = styled.textarea`
-  ${styles};
-`;
-const StyledSelect = styled<ISelectProps>(({ options = {}, ...props }) => (
+const StyledTextarea = styled<ITextProps>(({type = 'textarea', height = 100, ...props}) => (
+  <textarea {...props} />
+))`${styles}`;
+const StyledSelect = styled<ISelectProps>(({type = 'select', options = {}, ...props}) => (
   <select {...props}>
     {Object.keys(options).map(key => (
       <option key={key} value={key}>
@@ -73,20 +81,19 @@ const StyledSelect = styled<ISelectProps>(({ options = {}, ...props }) => (
 `;
 const StyledRadioOrCheckbox = styled<IRadioProps>(({ ...props }) => (
   <input {...props} />
-))`
-  ${styles};
-`;
-const StyledInput = styled.input`
-  ${styles};
-`;
+))`${styles};`;
+const StyledInput = styled<ITextProps>(({ ...props }) => (
+  <input {...props} />
+))`${styles};`;
 
-export default ({ height = DEFAULT_HEIGHT, ...props }: TInputPropsAll) => {
-  if (props.type === 'textarea') {
-    return <StyledTextarea {...props} height={height} />;
-  } else if (props.type === 'select') {
-    return <StyledSelect {...props} height={height} />;
-  } else if (props.type === 'radio' || props.type === 'checkbox') {
-    return <StyledRadioOrCheckbox {...props} height={height} />;
+export default ({type = DEFAULT_TYPE, ...props}: TInputPropsAll) => {
+  if (type === 'textarea') {
+    return <StyledTextarea {...props} />;
+  } else if (type === 'select') {
+    return <StyledSelect {...props as ISelectProps} />;
+  } else if (type === 'radio' || type === 'checkbox') {
+    return <StyledRadioOrCheckbox {...props} type={type} />;
+  } else {
+    return <StyledInput {...props} type={type} />;
   }
-  return <StyledInput {...props} height={height} />;
 };
