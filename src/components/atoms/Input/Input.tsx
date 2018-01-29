565da99d82d069216ba5201ac71a60cb4ed22e38
Eric Zeng <leizeng@leizeng.local>
2018-01-29 18:09:54 +0800

    refactor

--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -1,61 +1,34 @@
 import * as React from 'react';
-import styled, { css } from 'styled-components';
 
 import {
-  IInputProps,
   ITextProps,
   StyledInput,
   StyledTextarea,
-  baseInputStyles,
 } from './InputBase';
 
-interface ISelectProps extends IInputProps {
-  type?: 'select';
-  options: {
-    [key: string]: string;
-  };
-}
-interface IRadioProps extends IInputProps {
-  type?: 'radio' | 'checkbox';
-  checked?: boolean;
-}
-type TInputPropsAll = ISelectProps | IRadioProps | ITextProps;
+import StyledSelect, {
+  ISelectProps,
+} from './Select';
 
-const radioAndCheckboxStyle = css`
-&[type=checkbox], &[type=radio] {
-  display: inline-block;
-  border: 0;
-  border-radius: 0;
-  width: auto;
-  height: auto;
-  margin: 0 0.2rem 0 0;
-}`;
-const StyledSelect = styled(({type = 'select', options = {}, ...props}: ISelectProps) => (
-  <select {...props}>
-    {Object.keys(options).map(key => (
-      <option key={key} value={key}>
-        {options[key]}
-      </option>
-    ))}
-  </select>
-))`
-${baseInputStyles};
-${radioAndCheckboxStyle};
-`;
-const StyledRadioOrCheckbox = styled((props: IRadioProps) => (
-  <input {...props} />
-))`
-${baseInputStyles};
-${radioAndCheckboxStyle};
-`;
+import StyledRadio, {
+  IRadioProps,
+} from './Radio';
+
+import StyledCheckbox, {
+  ICheckboxProps,
+} from './Checkbox';
+
+type TInputPropsAll = ISelectProps | IRadioProps | ITextProps | ICheckboxProps;
 
 export default (props: TInputPropsAll) => {
   if (props.type === 'textarea') {
     return <StyledTextarea {...props as ITextProps} />;
   } else if (props.type === 'select') {
     return <StyledSelect {...props as ISelectProps} />;
-  } else if (props.type === 'radio' || props.type === 'checkbox') {
-    return <StyledRadioOrCheckbox {...props as IRadioProps} />;
+  } else if (props.type === 'radio') {
+    return <StyledRadio {...props as IRadioProps} />;
+  } else if (props.type === 'checkbox') {
+    return <StyledCheckbox {...props as ICheckboxProps} />;
   } else {
     return <StyledInput {...props as ITextProps} />;
   }
